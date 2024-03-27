3ea01301e0e6ed948fc22e857b2a3db08430c428
Juan Figueroa <jupiter1@iastate.edu>
2024-03-27 13:02:51 -0500

    finished act 10

---
+++ Class Activities/Activity10/reactcatalogtailwind/src/Categories.js
@@ -0,0 +1,12 @@
+// Categories.js
+/**
+ * Author: Juan Figueroa
+ISU Netid : jupiter1@iastate.edu
+Date : March 27th, 2024
+**/
+export default [
+    "electronics",
+    "jewelery",
+    "men's clothing",
+    "women's clothing"
+  ];
